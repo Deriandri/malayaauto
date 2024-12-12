46ce46a4a0d4e55ad04f1fb211ffbe926774b763
Deriandri <deriandriananda82@gmail.com>
2024-12-12 19:19:46 +0700

    Update insshws.sh

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -2,9 +2,9 @@
 # Proxy For Edukasi & Imclass
 
 # Link Hosting Kalian
-SFVPN="https://scvpsv2.malayaacx.my.id/install"
+SFVPN="https://raw.githubusercontent.com/Deriandri/malayaauto/main//install"
 
-wget -O /usr/local/bin/ws-dropbear https://scvpsv2.malayaacx.my.id/sshws/ws-dropbear
+wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/Deriandri/malayaauto/main//sshws/ws-dropbear
 chmod +x /usr/local/bin/ws-dropbear
 
 # Installing Service
@@ -32,7 +32,7 @@ systemctl enable ws-dropbear.service
 systemctl start ws-dropbear.service
 systemctl restart ws-dropbear.service
 
-wget -O /usr/local/bin/ws-ovpn https://scvpsv2.malayaacx.my.id/sshws/ws-ovpn.py
+wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/Deriandri/malayaauto/main//sshws/ws-ovpn.py
 chmod +x /usr/local/bin/ws-ovpn
 
 # Installing Service
@@ -59,7 +59,7 @@ systemctl daemon-reload
 systemctl enable ws-ovpn
 systemctl restart ws-ovpn
 
-wget -O /usr/local/bin/ws-stunnel https://scvpsv2.malayaacx.my.id/sshws/ws-stunnel
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/Deriandri/malayaauto/main//sshws/ws-stunnel
 chmod +x /usr/local/bin/ws-stunnel
 
 # Installing Service
